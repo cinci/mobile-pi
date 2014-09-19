7021cf6ed34285204feb82c0625963772152e651
cinci <xcinci@gmail.com>
2014-09-19 20:20:46 +0200

    Generic request to server

--- server/java/src/main/java/pi4droid/server/service/DevGpioService.java
+++ server/java/src/main/java/pi4droid/server/service/DevGpioService.java
@@ -2,6 +2,7 @@ package pi4droid.server.service;
 
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
+import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import pi4droid.server.Application;
@@ -15,13 +16,16 @@ import pi4droid.server.Application;
 @Profile(Application.PROFILE_DEV)
 public class DevGpioService implements GpioService {
 
+    private static final Logger log = Logger.getLogger(DevGpioService.class);
+
     @Override
     public void setPinValue(Pin pin, PinState pinState) {
-
+        log.info("PIN: " + pin.getName() + " STATE: " + pinState.getName());
     }
 
     @Override
     public PinState getPinValue(Pin pin) {
+        log.info("Returning: LOW");
         return PinState.LOW;
     }
 }
